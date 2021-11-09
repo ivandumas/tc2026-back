0adc97c13bbdbf08fbe30bb05bd265c88c1b9d0f
Iván Alejandro Dumas Martínez <ivan.dumas@hotmail.com>
2021-11-09 12:56:09 -0600

    museo controller

---
+++ controllers/museos.js
@@ -0,0 +1,78 @@
+const path = require('path')
+const Museo = require('../utils/database').models.museo
+
+exports.postAgregarMuseo = (req, res) => {
+    console.log(req.body)
+    Museo.findOne({
+        where: {
+            nombre: req.body.nombre,
+            ubicacion: req.body.ubicacion,
+        },
+    }).then((found) => {
+        if (found === null) {
+            Museo.create(req.body)
+                .then((result) => {
+                    console.log('Museo agregado exitosamente')
+                    res.json({status: 200, estado: 'aceptado' })
+                })
+                .catch((err) => {
+                    console.log(err)
+                    res.json({ estado: 'error' })
+                })
+        } else {
+            console.log('El registro de este Museo ya existe')
+            res.json({ estado: 'ya existe' })
+        }
+    })
+}
+
+exports.getObtenerVideojuegos = (req, res) => {
+    Videojuego.findAll()
+        .then((videojuegos) => {
+            console.log(videojuegos)
+            res.json({status: 200, videojuegos})
+        })
+        .catch((err) => {
+            console.log(err)
+            res.json({status: 500, estado: 'error' })
+        })
+}
+
+exports.postBorrarVideojuego = (req, res) => {
+    console.log(req.body)
+    Videojuego.destroy({
+        where: {
+            id: req.body.id,
+        },
+    })
+        .then(() => {
+            console.log('Videojuego destruido'),
+                res.json({ estado: 'aceptado' })
+        })
+        .catch((err) => {
+            console.log(err)
+            res.json({ estado: 'error' })
+        })
+}
+
+exports.postActualizarVideojuego = (req, res) => {
+    console.log(req.body)
+    Videojuego.update(
+        {
+            nombre: req.body.nombre,
+        },
+        {
+            where: {
+                id: req.body.id,
+            },
+        }
+    )
+        .then(() => {
+            console.log('Videojuego actualizado')
+            res.json({ estado: 'aceptado' })
+        })
+        .catch((err) => {
+            console.log(err)
+            res.json({ estado: 'error' })
+        })
+}
