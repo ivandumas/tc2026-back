676f61c42e323d93725e4cd24a6ad20d6b5b174c
Iván Alejandro Dumas Martínez <ivan.dumas@hotmail.com>
2021-11-08 20:19:40 -0600

    update of models + relationship

---
+++ utils/relations.js
@@ -0,0 +1,28 @@
+function applyRelationship(sequelize) {
+    //imprimir los modelos adheridos al objeto de con
+    console.log(sequelize.models)
+    const Museo = sequelize.models.museo
+    const Pelicula = sequelize.models.pelicula
+    const Playlist = sequelize.models.playlist
+
+    //Un videojuego puede jugarse en una o muchas consolas
+    Videojuego.belongsToMany(Consola,{through:ConsolaVideojuego}) //Crea una tabla intermedia y genera la llave foranea dentro de ella
+    //Una consola tiene uno o muchos videojuegos
+    Consola.belongsToMany(Videojuego,{through:ConsolaVideojuego}) //Crea una tabla intermedia y genera la llave foranea dentro de ella
+
+    //Si tuvieras una relacion uno a muchos
+    /*Supuesto obtener una tabla trofeo 
+    un videojuego puede tener muchos trofeos
+    PERO un trofeo solo pertenece a un videojuego*/
+
+    /* uno a muchos
+    Videojuego.hasMany(Trofeo)
+    */
+
+    /* muchos a uno
+    Trofeo.belongsToMany(Videojuego)
+    */
+}
+
+
+module.exports = {applyRelationship}
